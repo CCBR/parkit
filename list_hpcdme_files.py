082fff491e580e441023d2cc141b3fb0255b23af
Vishal Koparde <kopardev@users.noreply.github.com>
2022-06-16 21:45:14 -0400

    error checking

--- list_hpcdme_files.py
+++ list_hpcdme_files.py
@@ -10,12 +10,30 @@ Usage:
 __author__ = 'Vishal Koparde'
 __email__ = 'vishal.koparde@nih.gov'
 
+import argparse
+import os
+import json
+import uuid
+
+def _cmd_exists(cmd, path=None):
+    """ test if path contains an executable file with name
+    """
+    if path is None:
+        path = os.environ["PATH"].split(os.pathsep)
+
+    for prefix in path:
+        filename = os.path.join(prefix, cmd)
+        executable = os.access(filename, os.X_OK)
+        is_not_directory = os.path.isfile(filename)
+        if executable and is_not_directory:
+            return True
+    return False
 
 def collect_args():
     """
     Collect all the parsed arguments and return the parser
     """
-    import argparse
+    # import argparse
 
     parser = argparse.ArgumentParser(description='List.')
     parser.add_argument('-p', dest='archivepath', required=True,
@@ -33,9 +51,9 @@ def create_json(args):
     """
     Create the json required for query
     """
-    import json
-    import uuid
-    import os
+    # import json
+    # import uuid
+    # import os
     queryDict = dict()
     queryDict['page'] = 1
     queryDict['totalCount'] = True
@@ -60,14 +78,30 @@ def create_json(args):
     out_file.close()
     return json_file_path
             
+def cleanup(files2delete):
+    """
+    Deletes all the files in the provided list.
+    """
+    # import os
+    for file in files2delete:
+        os.remove(file)
 
 
 def main():
 
+    # files to delete ... make a list
+    delfiles = []
+    # check if HPCDME set up correctly
+    if not _cmd_exists('dm_query_dataobject'):
+        exit('HPCDMEAPIs are not setup correctly! dm_query_dataobject is not in PATH.')
     # Collect args 
     args = collect_args()
-    json = create_json(args)
+    # Create the query json
+    qjson = create_json(args)
+    delfiles.append(qjson)
     print(json)
+    # delete temp files
+    # cleanup(delfiles)
 
 
 if __name__ == '__main__':
