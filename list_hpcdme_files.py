e399188f9a86301a0737e5d027d8dba68218aacf
Vishal Koparde <kopardev@users.noreply.github.com>
2022-06-16 21:32:28 -0400

    create json

--- list_hpcdme_files.py
+++ list_hpcdme_files.py
@@ -20,18 +20,54 @@ def collect_args():
     parser = argparse.ArgumentParser(description='List.')
     parser.add_argument('-p', dest='archivepath', required=True,
         help='Archive Path')
-    parser.add_argument('-t', dest='filetype', required=False,
+    parser.add_argument('-f', dest='filetype', required=False,
         help='Extension of files to output eg. FASTQ')
+    parser.add_argument('-t', dest='tmpdir', required=False, default = '/dev/shm',
+        help='temp folder location')
+
 
     args = parser.parse_args()
     return args
 
+def create_json(args):
+    """
+    Create the json required for query
+    """
+    import json
+    import uuid
+    import os
+    queryDict = dict()
+    queryDict['page'] = 1
+    queryDict['totalCount'] = True
+    queryDict['detailedResponse'] = False
+    queryDict['compoundQuery'] = dict()
+    queryDict['compoundQuery']['operator'] = 'AND'
+    queryDict['compoundQuery']['queries'] = list()
+    query1 = dict()
+    query1['attribute'] = 'archive_file_id'
+    query1['operator'] = 'LIKE'
+    query1['value'] = '%'
+    queryDict['compoundQuery']['queries'].append(query1)
+    if not args.filetype:
+        query2['attribute'] = 'file_type'
+        query2['operator'] = 'EQUAL'
+        query2['value'] = 'FASTQ'
+        queryDict['compoundQuery']['queries'].append(query2)
+    
+    json_file_path = args.tmpdir+os.sep+str(uuid.uuid4())+".json"
+    out_file = open(json_file_path, "w")
+    json.dump(queryDict, out_file, indent = 6)
+    out_file.close()
+    return json_file_path
+            
 
 
 def main():
 
     # Collect args 
     args = collect_args()
+    json = create_json(args)
+    print(json)
 
 
 if __name__ == '__main__':
