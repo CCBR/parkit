6f53d62e18017f876eddf162b034ab1aedc22950
kopardev <vishal.koparde@nih.gov>
2024-03-22 10:06:41 -0400

    feat: adding version_check

---
+++ src/VersionCheck.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+global __version__
+current_path = os.path.dirname(os.path.abspath(__file__))
+vfile = open(os.path.join(current_path, "VERSION"), "r")
+__version__ = "v" + vfile.read()
+__version__ = __version__.strip()
+vfile.close()
