e6c72156dd37d328fd6716c3b764d7439bdef869
kopardev <vishal.koparde@nih.gov>
2024-03-27 14:57:51 -0400

    fix: delete unwanted

--- src/VersionCheck.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-global __version__
-current_path = os.path.dirname(os.path.abspath(__file__))
-vfile = open(os.path.join(current_path, "VERSION"), "r")
-__version__ = "v" + vfile.read()
-__version__ = __version__.strip()
-vfile.close()
